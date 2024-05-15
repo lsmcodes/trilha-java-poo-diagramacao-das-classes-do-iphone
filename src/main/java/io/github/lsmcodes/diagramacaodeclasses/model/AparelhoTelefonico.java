9c4d7257e3a73d693675bf067ab06d8f67c5717d
Lorena Medeiros <lomedeiros@proton.me>
2024-05-15 17:38:43 -0300

    feat: add AparelhoTelefonico interface to represent a phone

---
+++ src/main/java/io/github/lsmcodes/diagramacaodeclasses/model/AparelhoTelefonico.java
@@ -0,0 +1,9 @@
+package io.github.lsmcodes.diagramacaodeclasses.model;
+
+public interface AparelhoTelefonico {
+        
+        public void ligar();
+        public void atender();
+        public void iniciarCorreioVoz();
+
+}
