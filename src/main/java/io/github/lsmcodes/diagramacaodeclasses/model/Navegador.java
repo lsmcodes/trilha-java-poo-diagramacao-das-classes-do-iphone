e800b3e03c7b364dc2ee9ec281b6b60ba2d7b3a1
Lorena Medeiros <lomedeiros@proton.me>
2024-05-15 17:40:19 -0300

    feat: add Navegador interface to represent a web browser

---
+++ src/main/java/io/github/lsmcodes/diagramacaodeclasses/model/Navegador.java
@@ -0,0 +1,9 @@
+package io.github.lsmcodes.diagramacaodeclasses.model;
+
+public interface Navegador {
+
+        public void exibirPagina();
+        public void adicionarNovaAba();
+        public void atualizarPagina();
+
+}
