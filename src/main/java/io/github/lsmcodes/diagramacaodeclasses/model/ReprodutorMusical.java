df52d66bba0aeccb1f4128f7f8a50200e0b6d311
Lorena Medeiros <lomedeiros@proton.me>
2024-05-15 17:39:44 -0300

    feat: add ReprodutorMusical to represent a music player

---
+++ src/main/java/io/github/lsmcodes/diagramacaodeclasses/model/ReprodutorMusical.java
@@ -0,0 +1,9 @@
+package io.github.lsmcodes.diagramacaodeclasses.model;
+
+public interface ReprodutorMusical {
+        
+        public void tocar();
+        public void pausar();
+        public void selecionarMusica();
+
+}
